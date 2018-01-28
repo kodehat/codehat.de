0690e88440ea3064daff61e191f4f23a3f568fa8
Pixelhash <dev@codehat.de>
2018-01-28 13:15:21 +0100

    Add 'Projects' to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../pages/Home';
+import Projects from '../pages/Projects';
 import Contact from '../pages/Contact';
 
 Vue.use(Router);
@@ -11,6 +12,11 @@ export default new Router({
             path: '/',
             name: 'Home',
             component: Home,
+        },
+        {
+            path: '/projects',
+            name: 'Projects',
+            component: Projects,
         },
         {
             path: '/contact',
