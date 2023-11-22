f98dd2e57342c87b3bb5df163c3fce1f2bb275c4
kodehat <dev@codehat.de>
2023-11-22 13:48:43 +0100

    Fix some indentations

--- static/main.js
+++ static/main.js
@@ -18,7 +18,7 @@ let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, sys
 
 console.log(`Current theme is ${currentThemeSetting}`);
 
-// target the button using the data attribute we added earlier
+// Target the button using the data attribute we added earlier.
 const button = document.querySelector("[data-theme-toggle]");
 const lightSwitchText = document.getElementById("light-switch-text");
 
@@ -30,20 +30,20 @@ button.checked = currentThemeSetting === 'light';
 button.addEventListener("click", () => {
   const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
 
-  // update the button text
+  // Update the button text.
   const newCta = newTheme === "dark" ? "🌞" : "🌚";
   lightSwitchText.innerText = newCta;  
 
-  // use an aria-label if you are omitting text on the button
-  // and using sun/moon icons, for example
+  // Use an aria-label if you are omitting text on the button
+  // and using sun/moon icons, for example.
   button.setAttribute("aria-label", newCta);
 
-  // update theme attribute on HTML to switch theme in CSS
+  // Update theme attribute on HTML to switch theme in CSS.
   document.querySelector("html").setAttribute("data-theme", newTheme);
 
-  // update in local storage
+  // Update in local storage.
   localStorage.setItem("theme", newTheme);
 
-  // update the currentThemeSetting in memory
+  // Update the currentThemeSetting in memory.
   currentThemeSetting = newTheme;
 });
